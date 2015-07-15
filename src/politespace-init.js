e1ac86b7e4a13558f23f940d9094c9beb5274b4f
John Bender <john.m.bender@gmail.com>
2015-07-15 14:30:39 -0400

    enable external control of the proxy

--- src/politespace-init.js
+++ src/politespace-init.js
@@ -19,18 +19,29 @@
 				polite.createProxy();
 			}
 
+
 			$( this )
-				.bind( "politespace-update", function() {
-					polite.update();
-					polite.updateProxy();
+				.bind( "politespace-hide-proxy", function() {
+					$( this ).closest( ".politespace-proxy" ).removeClass( "active" );
+				})
+				.bind( "politespace-show-proxy", function() {
+					$( this ).closest( ".politespace-proxy" ).addClass( "active" );
+
+					if( polite.useProxy() ) {
+						polite.update();
+						polite.updateProxy();
+					}
 				})
 				.bind( "input keydown", function() {
 					polite.updateProxy();
 				})
 				.bind( "blur", function() {
-					$( this ).closest( ".politespace-proxy" ).addClass( "active" );
 					polite.update();
-					polite.updateProxy();
+
+					if( polite.useProxy() ){
+						$( this ).closest( ".politespace-proxy" ).addClass( "active" );
+						polite.updateProxy();
+					}
 				})
 				.bind( "focus", function() {
 					$( this ).closest( ".politespace-proxy" ).removeClass( "active" );
