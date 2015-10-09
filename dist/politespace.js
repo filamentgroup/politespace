37d09775b6db3e7fd88a14c4b9a01c55cf2921a8
Yann Leflour <yannl@bamlab.fr>
2015-10-09 14:42:14 +0200

    Added dist for bower

---
+++ dist/politespace.js
@@ -0,0 +1,230 @@
+/*! politespace - v0.1.6b - 2015-10-09
+Politely add spaces to input values to increase readability (credit card numbers, phone numbers, etc).
+ * https://github.com/filamentgroup/politespace
+ * Copyright (c) 2015 Filament Group (@filamentgroup)
+ * MIT License */
+
+(function( w, $ ){
+	"use strict";
+
+	var escapeRegExp = function(text) {
+		return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	};
+
+	var Politespace = function( element ) {
+		if( !element ) {
+			throw new Error( "Politespace requires an element argument." );
+		}
+
+		if( !element.getAttribute ) {
+			// Cut the mustard
+			return;
+		}
+
+		this.element = element;
+		this.$element = $( element );
+		this.prefix = this.$element.attr( "data-prefix" ) || "";
+		this.suffix = this.$element.attr( "data-suffix" ) || "";
+		this.delimiter = this.$element.attr( "data-delimiter" ) || " ";
+		// https://en.wikipedia.org/wiki/Decimal_mark
+		this.decimalMark = this.$element.attr( "data-decimal-mark" ) || "";
+		this.reverse = this.$element.is( "[data-reverse]" );
+		this.groupLength = this.$element.attr( "data-grouplength" ) || 3;
+
+		var proxyAnchorSelector = this.$element.attr( "data-proxy-anchor" );
+		this.$proxyAnchor = this.$element;
+		this.$proxy = null;
+
+		if( proxyAnchorSelector ) {
+			this.$proxyAnchor = this.$element.closest( proxyAnchorSelector );
+		}
+	};
+
+	Politespace.prototype._divideIntoArray = function( value ) {
+		var split = ( '' + this.groupLength ).split( ',' ),
+			isUniformSplit = split.length === 1,
+			dividedValue = [],
+			loopIndex = 0,
+			groupLength,
+			substrStart,
+			useCharCount;
+
+		while( split.length && loopIndex < value.length ) {
+			if( isUniformSplit ) {
+				groupLength = split[ 0 ];
+			} else {
+				// use the next split or the rest of the string if open ended, ala "3,3,"
+				groupLength = split.shift() || value.length - loopIndex;
+			}
+
+			// Use min if we’re at the end of a reversed string
+			// (substrStart below grows larger than the string length)
+			useCharCount = Math.min( parseInt( groupLength, 10 ), value.length - loopIndex );
+
+			if( this.reverse ) {
+				substrStart = -1 * (useCharCount + loopIndex);
+			} else {
+				substrStart = loopIndex;
+			}
+			dividedValue.push( value.substr( substrStart, useCharCount ) );
+			loopIndex += useCharCount;
+		}
+
+		if( this.reverse ) {
+			dividedValue.reverse();
+		}
+
+		return dividedValue;
+	};
+
+	Politespace.prototype.format = function( value ) {
+		var split;
+		var val = this.unformat( value );
+		var valSuffix = "";
+
+		if( this.decimalMark ) {
+			split = val.split( this.decimalMark );
+			valSuffix = split.length > 1 ? this.decimalMark + split[ 1 ] : "";
+			val = split[ 0 ];
+		}
+
+		return this.prefix + this._divideIntoArray( val ).join( this.delimiter ) + valSuffix + this.suffix;
+	};
+
+	Politespace.prototype.trimMaxlength = function( value ) {
+		var maxlength = this.element.getAttribute( "maxlength" );
+		// Note input type="number" maxlength does nothing
+		if( maxlength ) {
+			value = value.substr( 0, maxlength );
+		}
+		return value;
+	};
+
+	Politespace.prototype.getValue = function() {
+		return this.trimMaxlength( this.element.value );
+	};
+
+	Politespace.prototype.update = function() {
+		this.element.value = this.useProxy() || this.$element.attr( "type" ) === "password" ?
+			this.getValue() :
+			this.format( this.getValue() );
+	};
+
+	Politespace.prototype.unformat = function( value ) {
+		var val = value.replace( new RegExp(  this.delimiter, 'g' ), "" );
+		val = val.replace(new RegExp("^" + escapeRegExp(this.prefix), 'g'), "");
+		val = val.replace(new RegExp(escapeRegExp(this.suffix) + "$", 'g'), "");
+		return val;
+	};
+
+	Politespace.prototype.reset = function() {
+		this.element.value = this.unformat( this.element.value );
+	};
+
+	Politespace.prototype.useProxy = function() {
+		// this needs to be an attr check and not a prop for `type` toggling (like password)
+		return this.$element.attr( "type" ) === "number";
+	};
+
+	Politespace.prototype.updateProxy = function() {
+		if( this.useProxy() && this.$proxy.length ) {
+			this.$proxy.html( this.format( this.getValue() ) );
+			this.$proxy.css( "width", this.element.offsetWidth + "px" );
+		}
+	};
+
+	Politespace.prototype.createProxy = function() {
+		if( !this.useProxy() ) {
+			return;
+		}
+
+		var self = this;
+		function sumStyles( el, props ) {
+			var total = 0;
+			for( var j=0, k=props.length; j<k; j++ ) {
+				total += parseFloat( self.$element.css( props[ j ] ) );
+			}
+			return total;
+		}
+
+		var $el = $( "<div>" ).addClass( "politespace-proxy active" );
+		var $parent = this.$proxyAnchor.parent();
+
+		this.$proxy = $( "<div>" ).css({
+			font: this.$element.css( "font" ),
+			"padding-left": sumStyles( this.element, [ "padding-left", "border-left-width" ] ) + "px",
+			"padding-right": sumStyles( this.element, [ "padding-right", "border-right-width" ] ) + "px",
+			top: sumStyles( this.element, [ "padding-top", "border-top-width", "margin-top" ] ) + "px"
+		});
+		$el.append( this.$proxy );
+		$el.append( this.$proxyAnchor );
+		$parent.append( $el );
+
+		this.updateProxy();
+	};
+
+	w.Politespace = Politespace;
+
+}( this, jQuery ));
+
+(function( $ ) {
+	"use strict";
+
+	// jQuery Plugin
+
+	var componentName = "politespace",
+		initSelector = "[data-" + componentName + "]";
+
+	$.fn[ componentName ] = function(){
+		return this.each( function(){
+			var $t = $( this );
+			if( $t.data( componentName ) ) {
+				return;
+			}
+
+			var polite = new Politespace( this );
+			if( polite.useProxy() ) {
+				polite.createProxy();
+			}
+
+
+			$( this )
+				.bind( "politespace-hide-proxy", function() {
+					$( this ).closest( ".politespace-proxy" ).removeClass( "active" );
+					polite.update();
+				})
+				.bind( "politespace-show-proxy", function() {
+					$( this ).closest( ".politespace-proxy" ).addClass( "active" );
+
+					polite.update();
+					if( polite.useProxy() ) {
+						polite.updateProxy();
+					}
+				})
+				.bind( "input keydown", function() {
+					polite.updateProxy();
+				})
+				.bind( "blur", function() {
+					polite.update();
+
+					if( polite.useProxy() ){
+						$( this ).trigger( "politespace-show-proxy" );
+					}
+				})
+				.bind( "focus", function() {
+					$( this ).trigger( "politespace-hide-proxy" );
+					polite.reset();
+				})
+				.data( componentName, polite );
+
+			polite.update();
+		});
+	};
+
+	// auto-init on enhance (which is called on domready)
+	$( document ).bind( "enhance", function( e ) {
+		var $sel = $( e.target ).is( initSelector ) ? $( e.target ) : $( initSelector, e.target );
+		$sel[ componentName ]();
+	});
+
+}( jQuery ));
---
+++ dist/test/tests.js
@@ -0,0 +1,229 @@
+(function( w, $ ) {
+	"use strict";
+
+	function insertHtml( html ) {
+		var $fixture = $( "#qunit-fixture" );
+			$fixture.html( html );
+			$fixture.trigger( "enhance" );
+	}
+
+	module( "Defaults", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace>' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Not Reversed", function() {
+		strictEqual( this.pspace.reverse, false );
+	});
+
+	test( "Blur", function() {
+		$( this.pspace.element ).val( "12345678" ).trigger( "blur" );
+		equal( $( this.pspace.element ).val(), "123 456 78" );
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "12" );
+		equal( this.pspace.format( "123" ), "123" );
+		equal( this.pspace.format( "1234" ), "123 4" );
+		equal( this.pspace.format( "4444444444" ), "444 444 444 4" );
+	});
+
+	module( "Grouplength 2", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="2">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "12" );
+		equal( this.pspace.format( "123" ), "12 3" );
+		equal( this.pspace.format( "1234" ), "12 34" );
+		equal( this.pspace.format( "4444444444" ), "44 44 44 44 44" );
+	});
+
+	module( "Grouplength 1,2,", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="1,2,">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "1 2" );
+		equal( this.pspace.format( "123" ), "1 23" );
+		equal( this.pspace.format( "1234" ), "1 23 4" );
+		equal( this.pspace.format( "1234567890" ), "1 23 4567890" );
+	});
+
+	module( "Grouplength 1,2,3", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="1,2,3">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "1 2" );
+		equal( this.pspace.format( "123" ), "1 23" );
+		equal( this.pspace.format( "1234" ), "1 23 4" );
+		equal( this.pspace.format( "1234567890" ), "1 23 456" );
+	});
+
+	module( "Number type", {
+		setup: function() {
+			insertHtml( '<input type="number" data-politespace>' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Input and blur updates proxy", function() {
+		$( this.pspace.element ).val( "12345678" ).trigger( "input" );
+		equal( $( this.pspace.element ).val(), "12345678" );
+		equal( $( this.pspace.element ).prev().html(), "123 456 78" );
+	});
+
+	test( "Focus keeps value", function() {
+		$( this.pspace.element ).val( "12345678" ).trigger( "input" );
+		equal( $( this.pspace.element ).val(), "12345678" );
+		// We don’t test proxy html here because it isn’t shown and is irrelevant.
+	});
+
+	// Known issue in Firefox:
+	// Does not display leading zeros for input type="number" but .value returns
+	// leading zeros.
+	test( "Input with leading zero", function() {
+		$( this.pspace.element ).val( "01234" ).trigger( "input" );
+		equal( $( this.pspace.element ).val(), "01234" );
+		equal( $( this.pspace.element ).prev().html(), "012 34" );
+	});
+
+	module( "Custom delimiter", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-delimiter="-">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "123" ), "123" );
+		equal( this.pspace.format( "1234" ), "123-4" );
+		equal( this.pspace.format( "1234567890" ), "123-456-789-0" );
+	});
+
+	module( "Apply right to left, uniform grouplength", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="3" data-reverse data-delimiter="," data-decimal-mark=".">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Blur", function() {
+		$( this.pspace.element ).val( "12345678" ).trigger( "blur" );
+		equal( $( this.pspace.element ).val(), "12,345,678" );
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "12" );
+		equal( this.pspace.format( "123" ), "123" );
+		equal( this.pspace.format( "1234" ), "1,234" );
+		equal( this.pspace.format( "4444444444" ), "4,444,444,444" );
+		equal( this.pspace.format( "4444444444.44" ), "4,444,444,444.44" );
+	});
+
+	module( "Apply right to left, uniform grouplength, i18n Four Thousand = 4 000,00", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="3" data-reverse data-delimiter=" " data-decimal-mark=",">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "12" );
+		equal( this.pspace.format( "123" ), "123" );
+		equal( this.pspace.format( "1234" ), "1 234" );
+		equal( this.pspace.format( "4444444444" ), "4 444 444 444" );
+		equal( this.pspace.format( "4444444444,44" ), "4 444 444 444,44" );
+	});
+
+	module( "Apply right to left, nonuniform grouplength", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="4,3," data-reverse data-delimiter="-">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Blur", function() {
+		$( this.pspace.element ).val( "1112345678" ).trigger( "blur" );
+		equal( $( this.pspace.element ).val(), "111-234-5678" );
+	});
+
+	test( "Formats", function() {
+		equal( this.pspace.format( "12" ), "12" );
+		equal( this.pspace.format( "123" ), "123" );
+		equal( this.pspace.format( "12345" ), "1-2345" );
+		equal( this.pspace.format( "444444444444" ), "44444-444-4444" );
+	});
+
+	module( "Edit twice", {
+		setup: function() {
+			insertHtml( '<input type="text" data-politespace data-grouplength="3,3," data-delimiter="-">' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Blur", function() {
+		$( this.pspace.element ).val( "1112223333" ).trigger( "blur" );
+		equal( $( this.pspace.element ).val(), "111-222-3333" );
+		$( this.pspace.element ).trigger( "focus" ).trigger( "blur" );
+		equal( $( this.pspace.element ).val(), "111-222-3333" );
+	});
+
+	module( "Proxy Anchor", {
+		setup: function() {
+			insertHtml( '<div class="field"><input type="number" data-politespace data-proxy-anchor=".field"></div>' );
+			this.pspace = $( "[data-politespace]" ).data( "politespace" );
+		},
+		teardown: function() {
+			this.pspace = null;
+		}
+	});
+
+	test( "Test ancestor class names", function() {
+		equal( this.pspace.$element.parent().is( ".field" ), true, "First ancestor is the proxy anchor (.field)." );
+		equal( this.pspace.$element.parent().parent().is( ".politespace-proxy" ), true, "Second ancestor is the politespace proxy" );
+	});
+
+}( this, jQuery ));
