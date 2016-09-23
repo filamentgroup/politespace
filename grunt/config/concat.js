c4ae020498382d3f61aca040175429036d155228
Zach Leatherman <zachleatherman@gmail.com>
2016-09-23 16:06:24 -0500

    Adds `data-politespace-us-telephone` to automatically adjust maxlength if country code is entered and strip country code on blur (resetting maxlength to default). Fixes #71

--- grunt/config/concat.js
+++ grunt/config/concat.js
@@ -4,6 +4,7 @@ module.exports = {
 			"src/politespace.js",
 			"node_modules/creditable/creditablecardtype.js",
 			"src/politespace-creditcard.js",
+			"src/politespace-us-telephone.js",
 			"src/politespace-init.js"
 		]
 	},
---
+++ src/politespace-us-telephone.js
@@ -0,0 +1,38 @@
+// jQuery Plugin
+(function( w, $ ) {
+	"use strict";
+
+	function cleanup( el ) {
+		var $t = $( el );
+		$t.val( $t.val().replace( /^1/, "" ) );
+	}
+
+	$( document ).bind( "politespace-init politespace-input", function( event ) {
+		var $t = $( event.target );
+		if( !$t.is( "[data-politespace-us-telephone]" ) ) {
+			return;
+		}
+		var val = $t.val();
+
+		// Adjust maxlength
+		var maxlength= $t.attr( "maxlength" );
+		var maxlengthCacheKey = "politespace-us-telephone-maxlength";
+		var maxlengthCache = $t.data( maxlengthCacheKey );
+
+		if( maxlength && !maxlengthCache ) {
+			maxlengthCache = maxlength;
+			$t.data( maxlengthCacheKey, maxlength );
+
+			cleanup( $t[ 0 ] );
+			$t.one( "blur", function() {
+				$( this ).attr( "maxlength", maxlength );
+				cleanup( this );
+			});
+		}
+
+		if( val.indexOf( '1' ) === 0 ) {
+			$t.attr( "maxlength", parseInt( maxlengthCache, 10 ) + 1 );
+		}
+	});
+
+}( typeof global !== "undefined" ? global : this, jQuery ));
